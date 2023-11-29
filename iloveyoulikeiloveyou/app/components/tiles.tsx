b941a12fd4af4d03d3bbeacc59cc8fb86e62c4c0
Chinmayarulzz <chinmayarulzz@gmail.com>
2023-11-30 00:02:13 +0530

    Completion. Deployment Testing

--- iloveyoulikeiloveyou/app/components/tiles.tsx
+++ iloveyoulikeiloveyou/app/components/tiles.tsx
@@ -3,7 +3,7 @@ export default function Tile() {
         <div className="tile-container">
             <div>
                 <h2>Video Player</h2>
-                {/* <video controls src={"/Belief.mp4"} style={{ width: "400px", height: "400px" }}/> */}
+                <video controls src={"/Toftee.mp4"} style={{ width: "90vw", height: "75vh" }}/>
             </div>
         </div>
     );
